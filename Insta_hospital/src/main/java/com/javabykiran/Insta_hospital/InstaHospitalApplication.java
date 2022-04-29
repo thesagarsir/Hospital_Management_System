7b757b02f3a4d9820b58b0c0301739688f65b2e5
thesagarsir <74952980+thesagarsir@users.noreply.github.com>
2022-04-29 09:36:14 +0530

    initial commit
    initial commit

---
+++ Insta_hospital/src/main/java/com/javabykiran/Insta_hospital/InstaHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.javabykiran.Insta_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaHospitalApplication.class, args);
+	}
+
+}
---
+++ Insta_hospital/src/test/java/com/javabykiran/Insta_hospital/InstaHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabykiran.Insta_hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
