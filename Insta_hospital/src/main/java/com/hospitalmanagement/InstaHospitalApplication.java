5b63028694d45f56fa90278d345daf9805e52168
sagarjavabykiran <thesagarsir@gmail.com>
2022-04-29 09:44:51 +0530

    package updated Signed-off-by: sagarjavabykiran <thesagarsir@gmail.com>

--- Insta_hospital/src/main/java/com/hospitalmanagement/InstaHospitalApplication.java
+++ Insta_hospital/src/main/java/com/hospitalmanagement/InstaHospitalApplication.java
@@ -1,4 +1,4 @@
-package com.javabykiran.Insta_hospital;
+package com.hospitalmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
